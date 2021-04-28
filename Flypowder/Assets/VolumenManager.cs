bcc62c20e1b9ed2b99e2d16e4b23a7fb8a4a7d26
MiguelStern <mialnie@inf.upv.es>
2021-04-28 09:18:56 +0200

    volumen added

---
+++ Flypowder/Assets/VolumenManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumenManager : MonoBehaviour
+{
+    [SerializeField] string _volumeParameter = "MasterVolume";
+    [SerializeField] AudioMixer _mixer;
+    [SerializeField] Slider _slider;
+    private float _multiplier = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        _slider.onValueChanged.AddListener(HandleSliderValueChanged);
+    }
+
+    private void HandleSliderValueChanged(float value)
+    {
+        _mixer.SetFloat(_volumeParameter, Mathf.Log10(value) * _multiplier);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
