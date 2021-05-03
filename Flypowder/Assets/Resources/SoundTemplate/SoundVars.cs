ba4487fa5a225576106d5de68d3ab82ddbe57639
smugylooky <matiascuda@gmail.com>
2021-05-03 19:45:45 +0200

    ASD
    Me paso al portatil

---
+++ Flypowder/Assets/Resources/SoundTemplate/SoundVars.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SoundVars
+{
+    //MOVIMIENTO
+    public readonly static string SALTO = "jumping";
+    //ARMAS
+    public readonly static string DISPARO = "_shooting";
+    public readonly static string RECARGA = "_reloading";
+}
