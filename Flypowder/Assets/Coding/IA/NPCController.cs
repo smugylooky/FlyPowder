05a9cbd27d1e06823fd9a045bed7a1489a0cc1f7
Alberto Baixauli <albaiher99@gmail.com>
2021-05-18 02:01:16 +0200

    Añadida toda la logica respectiva a la IA, seguimiento como ataque

--- Flypowder/Assets/Coding/IA/NPCController.cs
+++ Flypowder/Assets/Coding/IA/NPCController.cs
@@ -8,34 +8,58 @@ public class NPCController : MonoBehaviour
     [SerializeField]
     private Stats npcStats;
     private Animator animator;
-    [SerializeField]
     private GameObject player;
+
+    private float minimalDistance = 3;
+
+    private float lastAttack = 0f;
+    private float timeOut = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();   
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerIsInRange() && HasSpentTimeOut()) 
+        {
+            AttackPlayer();
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D collision) 
+    private void AttackPlayer()
     {
-        if (collision.gameObject.layer == 9) 
+        lastAttack = Time.time;
+        //player.GetComponent<PlayerManager>().RecieveDamage(npcStats.Damage);
+        animator.SetTrigger("Attack");
+    }
+
+    private bool HasSpentTimeOut()
+    {
+        return Time.time - lastAttack > timeOut;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 9)
         {
             WeaponManager actualWeapon = player.GetComponentInChildren<WeaponManager>();
             ReciveDamage(5);
-            if (NPCIsDead()) 
+            if (NPCIsDead())
             {
                 this.gameObject.SetActive(false);
             }
         }
     }
 
+    private bool PlayerIsInRange() 
+    {
+        return Vector2.Distance(transform.position, player.transform.position) <= minimalDistance;
+    }
     private bool NPCIsDead()
     {
         return npcStats.Health <= 0;
